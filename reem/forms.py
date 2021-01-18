f4395e581ba163d72dec4501313f031c77cd9647
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-18 09:30:36 +0200

    added simple signup api

---
+++ reem/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password1', 'password2']
--- reem/views.py
+++ reem/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .forms import *
 
-# Create your views here.
+@api_view(['POST'])
+def api_signup(req):
+    form = SignupForm(req.POST)
+    if form.is_valid():
+        form.save()
+        return Response('Signup succeeded', status.HTTP_201_CREATED)
+
+    return Response(f'Signup failed\n{form.errors}', status.HTTP_406_NOT_ACCEPTABLE)
+
