257e48a8ae3c785dac82d9bf11c0d918d2c22381
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-18 09:13:03 +0200

    create the Incident model

--- reem/models.py
+++ reem/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Incident(models.Model):
+    time = models.DateTimeField(auto_now=True, editable=False)
+    symptoms = models.BigIntegerField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    diagnosis = models.CharField(max_length=100, null=True)
+
