c56dfa810a28a43b3813142bc0e179bd15e6ddd5
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-19 04:18:10 +0200

    made login and signup generate tokens and return them to the sender

--- reem/views.py
+++ reem/views.py
@@ -1,23 +1,25 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from .forms import *
 
 @api_view(['POST'])
 def api_signup(req):
     form = SignupForm(req.POST)
     if form.is_valid():
-        form.save()
-        return Response('Signup succeeded', status.HTTP_201_CREATED)
+        new_user = form.save()
+        new_token, _ = Token.objects.get_or_create(user=new_user)
+        return Response(f'Token {new_token}', status.HTTP_201_CREATED)
 
-    return Response(f'Signup failed\n{form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
+    return Response(f'{form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
 
 @api_view(['POST'])
 def api_login(req):
     form = LoginForm(req, req.POST)
     if form.is_valid():
-        return Response('Login succeeded', status.HTTP_202_ACCEPTED)
-    return Response(f'Login failed {form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
+        logged_user = form.get_user()
+        new_token, _ = Token.objects.get_or_create(user=logged_user)
+        return Response(f'Token {new_token}', status.HTTP_202_ACCEPTED)
+    return Response(f'{form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
 
