81294bd724c1a98d8f19bd3a62a487ac49827f2d
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-19 08:13:39 +0200

    added an api to get incidents

--- reem/urls.py
+++ reem/urls.py
@@ -5,4 +5,5 @@ app_name = 'reem'
 urlpatterns = [
     path('signup/', api_signup, name='signup'),
     path('login/', api_login, name='login'),
+    path('incident/get/<int:maxsize>/', api_get_incidents, name='get_incidents'),
 ]
--- reem/views.py
+++ reem/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from .forms import *
+from .models import *
+from .serializers import *
 
 @api_view(['POST'])
 def api_signup(req):
@@ -22,4 +26,14 @@ def api_login(req):
         new_token, _ = Token.objects.get_or_create(user=logged_user)
         return Response(f'Token {new_token}', status.HTTP_202_ACCEPTED)
     return Response(f'{form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def api_get_incidents(req, maxsize=50):
+    user_incidents = Incident.objects.filter(user=req.user)
+    count = len(user_incidents)
+    incidents = user_incidents[count - min(maxsize, count) : count]
+    serializer = IncidentSerializer(incidents, many=True)
+    return Response(serializer.data, status.HTTP_200_OK)
 
