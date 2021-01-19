9debe715e3cb2ebb50e8a291bc713f9981e89bbd
CoderEx24 <kareemhacker123321@gmail.com>
2021-01-19 03:04:51 +0200

    added basic crediential checking

--- reem/forms.py
+++ reem/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import *
 
 class SignupForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    pass
+
--- reem/views.py
+++ reem/views.py
@@ -1,3 +1,5 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,5 +12,12 @@ def api_signup(req):
         form.save()
         return Response('Signup succeeded', status.HTTP_201_CREATED)
 
-    return Response(f'Signup failed\n{form.errors}', status.HTTP_406_NOT_ACCEPTABLE)
+    return Response(f'Signup failed\n{form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
+
+@api_view(['POST'])
+def api_login(req):
+    form = LoginForm(req, req.POST)
+    if form.is_valid():
+        return Response('Login succeeded', status.HTTP_202_ACCEPTED)
+    return Response(f'Login failed {form.error_messages}', status.HTTP_406_NOT_ACCEPTABLE)
 
