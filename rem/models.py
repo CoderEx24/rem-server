bd86a8e839734c9db7f37218f1ab1b1b1e27fe88
CoderEx24 <kareemhacker123321@gmail.com>
2021-02-07 13:12:24 +0200

    changed symptoms from BigIntegerField to CharField

--- rem/models.py
+++ rem/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Incident(models.Model):
     time = models.DateTimeField(auto_now=True, editable=False)
-    symptoms = models.BigIntegerField()
+    symptoms = models.CharField(max_length=41)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     diagnosis = models.CharField(max_length=100, null=True)
 
